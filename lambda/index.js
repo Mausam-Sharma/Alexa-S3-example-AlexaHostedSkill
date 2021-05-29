e262f1ebc6c918ec1e8b7c69f50f14cb3ce2fd43
Mausam-Sharma <35280772+Mausam-Sharma@users.noreply.github.com>
2021-05-29 14:21:36 +0530

    s3 example

---
+++ lambda/index.js
@@ -0,0 +1,74 @@
+
+const Alexa = require('ask-sdk');
+const Util = require('./util.js');
+
+const LaunchRequestHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'LaunchRequest';
+    },
+    handle(handlerInput) {
+        
+        const audioUrl = Util.getS3PreSignedUrl("Media/silence.mp3").replace(/&/g,'&amp;');
+        const speakOutput ='<audio src="soundbank://soundlibrary/home/amzn_sfx_doorbell_chime_02"/>' +`<audio src="${audioUrl}"/>`+'darwaaze par <emphasis level="strong">koi</emphasis> toh hai';
+        const pictureUrl = Util.getS3PreSignedUrl("Media/taj.jpg")
+
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .withStandardCard('card title', 'card text', pictureUrl)
+            .withShouldEndSession(true)
+            .getResponse();
+    }
+};
+
+
+
+/* *
+ * SessionEndedRequest notifies that a session was ended. This handler will be triggered when a currently open 
+ * session is closed for one of the following reasons: 1) The user says "exit" or "quit". 2) The user does not 
+ * respond or says something that does not match an intent defined in your voice model. 3) An error occurs 
+ * */
+const SessionEndedRequestHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'SessionEndedRequest';
+    },
+    handle(handlerInput) {
+        console.log(`~~~~ Session ended: ${JSON.stringify(handlerInput.requestEnvelope)}`);
+        // Any cleanup logic goes here.
+        return handlerInput.responseBuilder.getResponse(); // notice we send an empty response
+    }
+};
+
+/**
+ * Generic error handling to capture any syntax or routing errors. If you receive an error
+ * stating the request handler chain is not found, you have not implemented a handler for
+ * the intent being invoked or included it in the skill builder below 
+ * */
+const ErrorHandler = {
+    canHandle() {
+        return true;
+    },
+    handle(handlerInput, error) {
+        const speakOutput = 'Sorry, I had trouble doing what you asked. Please try again.';
+        console.log(`~~~~ Error handled: ${JSON.stringify(error)}`);
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    }
+};
+
+/**
+ * This handler acts as the entry point for your skill, routing all request and response
+ * payloads to the handlers above. Make sure any new handlers or interceptors you've
+ * defined are included below. The order matters - they're processed top to bottom 
+ * */
+exports.handler = Alexa.SkillBuilders.custom()
+    .addRequestHandlers(
+        LaunchRequestHandler,
+        SessionEndedRequestHandler,
+        )
+    .addErrorHandlers(
+        ErrorHandler)
+    .lambda();
